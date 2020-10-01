abcbffa565daa8d9eddd06435df00711e62ec602
ANMOL BHARDWAJ <53622325+anmolbhardwaj17@users.noreply.github.com>
2020-10-02 01:11:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAjn5_b7gNngKa4quFjT-DOffnwPywmvnc",
-  authDomain: "facebook-messenger-clone-76343.firebaseapp.com",
-  databaseURL: "https://facebook-messenger-clone-76343.firebaseio.com",
-  projectId: "facebook-messenger-clone-76343",
-  storageBucket: "facebook-messenger-clone-76343.appspot.com",
-  messagingSenderId: "236208285772",
-  appId: "1:236208285772:web:3868a2d20867147afbece9",
-  measurementId: "G-M7CDR05GGF"
-});
+//write your firebase console config
 
 const db = firebaseApp.firestore();
 
